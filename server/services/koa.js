4959371d70754a0fe92b10cd0e4b75e656935099
zhanshichao <zhanshichaoFe@aliyun.com>
2019-04-08 15:22:38 +0800

    feat(test): koa

--- server/services/koa.js
+++ server/services/koa.js
@@ -1,17 +1,22 @@
 import Koa from 'koa'
-
-const app = new Koa()
 import logger from "../utils/logger"
 
+const app = new Koa()
+// middleware的顺序很重要，也就是调用app.use()的顺序决定了middleware的顺序
+
 // 将给定的中间件方法添加到此应用程序
-app.use(async ctx => {
-  const { request } = ctx
-  logger.info(request.query) // 获取get请求参数
-  ctx.body = 'Hello World'
+app.use(async (ctx, next) => {
+  const { request, response } = ctx
+  // logger.info(request.query) // 获取get请求参数
+  logger.info(ctx.query) // 也可以使用cxt简写，ctx.query == ctx.request.query 获取get请求参数
+  ctx.body = 'Hello World' // ctx.body == ctx.response.query
 })
 
 app.on('error', (err, ctx) => {
-  log.error('server error', err, ctx)
+  logger.error(`
+    server error: ${err}
+    ctx: ${ctx}
+  `)
 })
 
 export default app
